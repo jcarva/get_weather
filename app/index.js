70ce6d73332772d1edec9d4bea325c8470b0582f
Jaelson <jaelsonjunior99@gmail.com>
2016-07-19 15:49:56 -0300

    Create 'Hello Word' to component, just to a initial test.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+const Live = (props) => {
+  return (
+    <div>{props.name}</div>
+  )
+}
+
+class User extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render() {
+    return (
+        <Live {...this.props}/>
+    )
+  }
+}
+
+ReactDOM.render(<User name="Jota"/>, document.getElementById('app'));
