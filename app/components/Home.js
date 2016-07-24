da20fe68f2c73c0e61e6d536c00d103972d5784c
Jaelson <jaelsonjunior99@gmail.com>
2016-07-24 19:30:38 -0300

    Create home component.

---
+++ app/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class Home extends React.Component {
+  render() {
+    return(
+      <div id="home">
+        <h1>TEST</h1>
+      </div>
+    )
+  }
+}
