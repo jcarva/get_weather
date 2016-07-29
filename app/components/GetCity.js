6b0fe2509c49e7e74100e1647a29d2a9d74b87bd
Jaelson <jaelsonjunior99@gmail.com>
2016-07-29 16:37:04 -0300

    Create private components inside GetCity presentation component.
    Add style to the privates components.

--- app/components/GetCity.js
+++ app/components/GetCity.js
@@ -1,8 +1,24 @@
 import React from 'react'
 
+const Button = () => {
+  return(
+    <button className="btn btn-lg btn-warning">Get Weather</button>
+  )
+}
+
+const InputField = () => {
+  return(
+    <input className='form-control' placeholder='San Diego, California' type='text'/>
+  )
+}
+
 const GetCity = (props) => {
   return(
-    <h1>Get City Component</h1>
+    <div className="text-center">
+      <InputField/>
+      <br/>
+      <Button/>
+    </div>
   )
 }
 
