e85cbf257a3154ea340744b51ac1359580b51694
Jaelson <jaelsonjunior99@gmail.com>
2016-07-29 20:58:23 -0300

    Add cover image in Home component.

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,7 +5,7 @@ const Home = (props) => {
     return(
       <div id="home">
         <a href="http://www.nytimes.com/weather/">
-          <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="sun_cloud_image" title="Weather App"/>
+          <img style={{maxWidth: 300}} src="/app/assets/images/cover.png" alt="sun_cloud_image" title="Weather App"/>
         </a>
         <h1>Enter a City and State</h1>
         <GetCityContainer/>
