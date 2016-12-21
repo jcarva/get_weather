9cb7f2db652cc77b4d8593d642fbffa45cc16f71
Jaelson <jaelsonjunior99@gmail.com>
2016-12-20 21:49:33 -0300

    Create a route to city forecasts

--- app/config/Routes.js
+++ app/config/Routes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Router, Route, IndexRoute, browserHistory } from 'react-router'
 import Main from '../containers/Main'
 import HomeContainer from '../containers/HomeContainer'
+import ForecastContainer from '../containers/ForecastContainer'
 
 export default class Routes extends React.Component {
   render() {
@@ -9,6 +10,7 @@ export default class Routes extends React.Component {
       <Router history={browserHistory}>
         <Route path="/" component={Main}>
           <IndexRoute component={HomeContainer}/>
+          <Route path='forecast/:city' component={ForecastContainer} />
         </Route>
       </Router>
     )
