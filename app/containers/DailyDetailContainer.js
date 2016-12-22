f00f7064d3d9c11a3e647bb3028596a4f3ec0317
Jaelson <jaelsonjunior99@gmail.com>
2016-12-22 15:06:46 -0300

    Create DailyDetailContainer component

---
+++ app/containers/DailyDetailContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import DailyDetail from '../components/DailyDetail'
+
+export default class DailyDetailContainer extends React.Component {
+  render() {
+    return(
+      <DailyDetail/>
+    )
+  }
+}
